c2f2ce81f22dd0b0ad65f4f94585e0404c27f4b9
pratikmusmade <pratikmusmadessjgod@gmail.com>
2024-04-18 15:21:04 +0530

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL,import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  console.log(import.meta.env.VITE_ggg);
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
